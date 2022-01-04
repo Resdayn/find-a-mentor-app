0bada25340af55c3fba24c7112b2bf9cb5266a84
Juan Gonzalez <jgonzalez@naver.com>
2022-01-04 16:50:56 +0900

    updated

--- src/router.js
+++ src/router.js
@@ -1,16 +1,23 @@
-import {createRouter, createWebHistory} from "vue-router ";
+import {createRouter, createWebHistory} from "vue-router";
+
+import MentorDetails from './components/pages/mentors/MentorDetails.vue';
+import MentorRegistration from './components/pages/mentors/MentorRegistration.vue';
+import MentorsList from './components/pages/mentors/MentorsList.vue';
+import ContactMentor from './components/pages/requests/ContactMentor.vue';
+import RequestsReceived from './components/pages/requests/RequestsReceived.vue';
+import NotFound from './components/pages/NotFound.vue';
 
 const router = createRouter({
     history: createWebHistory(), 
     routes: [
         {path: '/', redirect: '/mentors'},
-        {path: '/mentors', component: null},
-        {path: '/mentors/:id', component: null, children: [
-           {path: 'contact', component: null}, // This will load the contact page for a specific mentor
+        {path: '/mentors', component: MentorsList},
+        {path: '/mentors/:id', component: MentorDetails, children: [
+           {path: 'contact', component: ContactMentor}, // This will load the contact page for a specific mentor
         ]},
-        {path: '/register', component: null},
-        {path: '/requests', component: null},
-        {path: '/:notFound(.*)', component: null}
+        {path: '/register', component: MentorRegistration},
+        {path: '/requests', component: RequestsReceived},
+        {path: '/:notFound(.*)', component: NotFound }
         
     ]
 });
