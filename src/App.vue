e7d37ee7febced128888320812b161fcb4027732
Juan Gonzalez <jgonzalez@naver.com>
2022-01-04 16:51:11 +0900

    updated

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,27 @@
 <template>
-
+  <the-header></the-header>
+  <router-view></router-view>
 </template>
 
 <script>
-
-
+import TheHeader from "../src/components/layout/TheHeader.vue";
 export default {
-  name: 'App',
-}
+  components: {
+    TheHeader,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url("https://fonts.googleapis.com/css2?family=Titillium+Web&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Titillium Web", sans-serif;
 }
 </style>
