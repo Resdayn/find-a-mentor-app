140cd4f062bf2a95ef234c2606a48d9e7c7d358a
Juan Gonzalez <jgonzalez@naver.com>
2022-01-05 11:58:41 +0900

    updated

--- src/components/pages/mentors/MentorsList.vue
+++ src/components/pages/mentors/MentorsList.vue
@@ -3,21 +3,28 @@
         FILTER
     </section>
     <section>
-        <div class="control">
+        <div class="controls">
             <button>Refresh list</button>
             <router-link to='/register'>Register as Mentor</router-link>
         </div>
         <ul v-if="hasMentors">
-            <li v-for="mentor in filteredMentors" :key="mentor.id">
-                {{ mentor.firstName }}
-            </li>
+            <mentor-item v-for="mentor in filteredMentors" 
+            :key="mentor.id"
+            :id="mentor.id"
+            :firstName="mentor.firstName"
+            :lastName="mentor.lastName"
+            :rate="mentor.hourlyRate"
+            :areas="mentor.areas"
+            ></mentor-item> 
         </ul>
         <h3 v-else>There are no available mentors yet</h3>
     </section>
 </template> 
 
 <script>
+import MentorItem from '../../mentors/MentorItem.vue';
 export default {
+  components: { MentorItem },
     computed: {
         filteredMentors() {
             return this.$store.getters['mentors/mentors'];
@@ -28,3 +35,15 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+} 
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
