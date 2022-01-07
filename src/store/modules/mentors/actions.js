b92d9e60e9ae7b52a7da87a2b197762e8821dda8
Juan Gonzalez <jgonzalez@naver.com>
2022-01-07 15:48:07 +0900

    added fetch

--- src/store/modules/mentors/actions.js
+++ src/store/modules/mentors/actions.js
@@ -1,14 +1,30 @@
+import fireBaseToken from "../../../../token.js"
+
 export default {
-  registerMentor(context, data) {
+  async registerMentor(context, data) {
     // Used in MentorRegistration.vue to receive the data from the form and send it to the registerMentor mutation.
+    const userId = context.rootGetters.userId;
     const mentor = {
-      id: context.rootGetters.userId,
       firstName: data.first,
       lastName: data.last,
       areas: data.areas,
       description: data.desc,
       hourlyRate: data.rate,
     };
-    context.commit('registerMentor', mentor); 
+
+    // Sends to FireBase
+    const response = await fetch(`${fireBaseToken.token}/mentors/${userId}.json`, {
+      method: 'PUT', // PUT overwrites if there is no entry, POST always create new entries
+      body: JSON.stringify(mentor)
+    });
+
+    if(!response.ok) {
+      // throw error
+    }
+
+    context.commit('registerMentor', {
+      ...mentor,
+      id: userId // we add the user id here because is already in the root store and is not needed to be sent to the server
+    }); 
   },
 };
