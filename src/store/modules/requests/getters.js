a52efa5d353e27e9874a35a997b1660133774561
Juan Gonzalez <jgonzalez@naver.com>
2022-01-07 11:44:40 +0900

    updated

--- src/store/modules/requests/getters.js
+++ src/store/modules/requests/getters.js
@@ -1,3 +1,9 @@
 export default {
-    
+    requests(state) {
+        return state.requests;
+    },
+    hasRequests(state) {
+        //returns boolean according to the  expresion below
+        return state.requests && state.requests.length > 0;
+    }
 }
