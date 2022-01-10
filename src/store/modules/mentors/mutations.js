60b94f509ae325a27b3aa82c3b58977369c88ccb
Juan Gonzalez <jgonzalez@naver.com>
2022-01-10 12:22:36 +0900

    updated

--- src/store/modules/mentors/mutations.js
+++ src/store/modules/mentors/mutations.js
@@ -2,5 +2,9 @@ export default {
     registerMentor(state, payload) {
         // This receives the payload from the action in the correct format and adds it to the list of mentors.
         state.mentors.push(payload);
+    },
+    setMentors(state, payload) {
+        // puts the mentors from FireBase in the local Vuex store after fetched with the action
+        state.mentors = payload;
     }
 };
