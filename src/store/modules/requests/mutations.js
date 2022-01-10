f4ce63b8be3aaa8d3b3c2d976c03738359fc4302
Juan Gonzalez <jgonzalez@naver.com>
2022-01-10 15:41:57 +0900

    updated

--- src/store/modules/requests/mutations.js
+++ src/store/modules/requests/mutations.js
@@ -3,5 +3,9 @@
 export default {
     addRequest(state, payload) {
         state.requests.push(payload);
+    },
+    setRequests(state, payload){
+        // adds an array of requests to the vuex local storage for requests
+        state.requests = payload;
     }
 }
