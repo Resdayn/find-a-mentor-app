fd85d6bb076a867e9d6c32f6f6270fd1a3af3f82
Juan Gonzalez <jgonzalez@naver.com>
2022-01-17 12:42:39 +0900

    updated

---
+++ src/components/pages/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+    <section>
+        <base-card>
+            <h2>Page Not Found</h2>
+            <p>This page could not be found. Maybe check our list of <router-link to="/mentors"> mentors</router-link></p>
+        </base-card>
+    </section>
+</template>
