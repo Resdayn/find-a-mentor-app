e3e454e27b5e25ed3ebafc99b4e1fe7580bb1241
Juan Gonzalez <jgonzalez@naver.com>
2022-01-18 22:52:00 +0900

    moved user related state & getters to Vuex authModule

--- src/store/index.js
+++ src/store/index.js
@@ -2,22 +2,14 @@ import { createStore } from "vuex";
 
 import mentorsModule from "./modules/mentors/index";
 import requestModule from "./modules/requests/index";
+import authModule from "./modules/auth/index.js";
 
 const store = createStore({
-  modules: { 
-      mentors: mentorsModule,
-      requests: requestModule
-    },
-    state() {
-      return {
-        userId: 'c3'
-      }
-    },
-    getters: {
-       userId(state) {
-        return state.userId;
-       }
-    }
+  modules: {
+    mentors: mentorsModule,
+    requests: requestModule,
+    auth: authModule,
+  },
 });
 
 export default store;
