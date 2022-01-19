ba5d7920b7c2c754ba88ed516e22f69bb81c8607
Juan Gonzalez <jgonzalez@naver.com>
2022-01-19 09:22:44 +0900

    added user visual input & error handling

--- src/components/pages/auth/UserAuth.vue
+++ src/components/pages/auth/UserAuth.vue
@@ -1,54 +1,67 @@
 <template>
-  <base-card>
-    <form @submit.prevent="submitForm">
-      <div class="form-control">
-        <label for="email">Email</label>
-        <input type="email" id="email" v-model.trim="email" />
-      </div>
-      <div class="form-control">
-        <label for="password">Password</label>
-        <input type="password" id="password" v-model.trim="password" />
-      </div>
-      <p v-if="!formIsValid">
-        Please enter a valid email and password (must be at least 6 characters
-        long).
-      </p>
-      <base-button>{{submitButtonCaption}}</base-button>
-      <base-button type="button" mode="flat" @click="switchAuthMode"
-        >{{switchModeButtonCaption}}</base-button
-      >
-    </form>
-  </base-card>
+  <div>
+    <base-dialog :show="!!error" title="An error ocurred" @close="handleError">
+      <p>{{error}}</p>
+    </base-dialog>
+    <base-dialog :show="isLoading" title="Authenticating..." fixed>
+      <base-spinner></base-spinner>
+    </base-dialog>
+    <base-card>
+      <form @submit.prevent="submitForm">
+        <div class="form-control">
+          <label for="email">Email</label>
+          <input type="email" id="email" v-model.trim="email" />
+        </div>
+        <div class="form-control">
+          <label for="password">Password</label>
+          <input type="password" id="password" v-model.trim="password" />
+        </div>
+        <p v-if="!formIsValid">
+          Please enter a valid email and password (must be at least 6 characters
+          long).
+        </p>
+        <base-button>{{ submitButtonCaption }}</base-button>
+        <base-button type="button" mode="flat" @click="switchAuthMode">{{
+          switchModeButtonCaption
+        }}</base-button>
+      </form>
+    </base-card>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      email: "",
+     email: "",
       password: "",
       formIsValid: true,
       mode: "login", // or 'signup'
+      isLoading: false,
+      error: null,
     };
   },
   computed: {
     submitButtonCaption() {
-      if (this.mode === 'login') {
-        return 'Login'
+      if (this.mode === "login") {
+        return "Login";
       } else {
-        return 'Sign Up'
+        return "Sign Up";
       }
     },
     switchModeButtonCaption() {
-      if (this.mode === 'login') {
-        return 'Sign Up instead'
+      if (this.mode === "login") {
+        return "Sign Up instead";
       } else {
-        return 'Login instead'
+        return "Login instead";
       }
+    },
+    handleError() {
+      this.error = null;
     }
   },
   methods: {
-    submitForm() {
+    async submitForm() {
       // Form Validation
       this.formIsValid = true;
       if (
@@ -59,21 +72,32 @@ export default {
         this.formIsValid = false;
         return;
       }
+
+      // Start loading Spinner
+      this.isLoading = true;
+
       // Send HTTP request
-      if (this.mode === 'login') {
-        // ...
-      } else {
-        this.$store.dispatch('signup', {
-          email: this.email,
-          password: this.password
-        })
+      try {
+        if (this.mode === "login") {
+          // ...
+        } else {
+          await this.$store.dispatch("signup", {
+            email: this.email,
+            password: this.password,
+          });
+        }
+      } catch (error) {
+        this.error =
+          error.message || "Failed to authenticate, please try later";
       }
+      // Stop loading Spinner
+      this.isLoading = false;
     },
     switchAuthMode() {
-      if (this.mode=== 'login') {
-        this.mode = 'signup';
+      if (this.mode === "login") {
+        this.mode = "signup";
       } else {
-        this.mode = 'login';
+        this.mode = "login";
       }
     },
   },
