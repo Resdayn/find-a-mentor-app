d8775cbb67ddeb306d5809fbd4639ca14499169c
Juan Gonzalez <jgonzalez@naver.com>
2022-01-19 17:25:47 +0900

    fixed bug when adding the userId to the store

--- src/store/modules/auth/mutations.js
+++ src/store/modules/auth/mutations.js
@@ -2,7 +2,7 @@ export default {
     setUser(state, payload) {
         // Adds the new user to the Store
         state.token = payload.token;
-        state.userId = payload.userI;
+        state.userId = payload.userId;
         state.tokenExpiration = payload.tokenExpiration;
     }
 }
