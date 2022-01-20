8fe4b6fd6e9e25c0d608100c42c9135b2698c162
Juan Gonzalez <jgonzalez@naver.com>
2022-01-20 11:45:30 +0900

    added setAutoLogout

--- src/store/modules/auth/mutations.js
+++ src/store/modules/auth/mutations.js
@@ -3,6 +3,9 @@ export default {
         // Adds the new user to the Store
         state.token = payload.token;
         state.userId = payload.userId;
-        state.tokenExpiration = payload.tokenExpiration;
+        state.didAutoLogout = false;
+    },
+    setAutoLogout(state) {
+        state.didAutoLogout = true;
     }
 }
