f5e0483f2ed0fdbd109c4c4c598571d3f092646a
Juan Gonzalez <jgonzalez@naver.com>
2022-01-20 11:44:41 +0900

    added autologin/autologout and timers

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,4 +1,7 @@
 import firebaseKey from "../../../../token";
+
+let timer; // Holds a global timer that can be later deactivated by other functions.
+
 export default {
   async login(context, payload) {
     // returns the promised returned by the auth dispatch so it can be tracked in the component for error handling
@@ -18,13 +21,16 @@ export default {
 
   logout(context) {
     // Remove the local store if any.
-    localStorage.removeItem('token');
-    localStorage.removeItem('userId');
+    localStorage.removeItem("token");
+    localStorage.removeItem("userId");
+    localStorage.removeItem("tokenExpiration");
+
+    // Clear the autologout timer
+    clearTimeout(timer);
 
     context.commit("setUser", {
       userId: null,
       token: null,
-      tokenExpiration: null,
     });
   },
 
@@ -37,7 +43,7 @@ export default {
       url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${firebaseKey.apiKey}`;
     }
 
-    //HTTP request 
+    //HTTP request
 
     const response = await fetch(url, {
       method: "POST",
@@ -60,28 +66,57 @@ export default {
     }
 
     // Save data to local storage
+    const tokenExpiresIn = +responseData.expiresIn * 1000; // changes the string to integer (the +) and converts it to milisecs
+    // const tokenExpiresIn = 5000; // FOR TESTING
+    const tokenExpirationDate = new Date().getTime() + tokenExpiresIn; // Calculates the date by multiplying current time in milisec by duration
 
-    localStorage.setItem('token', responseData.idToken);
-    localStorage.setItem('userId', responseData.localId);
+    localStorage.setItem("token", responseData.idToken);
+    localStorage.setItem("userId", responseData.localId);
+    localStorage.setItem("tokenExpiration", tokenExpirationDate);
+
+    // Sets a timer to auto-logout the user
+    timer = setTimeout(() => {
+      context.dispatch("autoLogout");
+    }, tokenExpiresIn);
 
     // If everything is successfull, commits the setUser mutation to save the server data in Vuex
     context.commit("setUser", {
       token: responseData.idToken,
       userId: responseData.localId,
-      tokenExpiration: responseData.expiresIn,
     });
   },
+
   autoLogin(context) {
-    // Retrieves the token and userId from local storage and commits setUser with it. If not, autologin will not be executed.
-    const token = localStorage.getItem('token');
-    const userId = localStorage.getItem('userId');
+    // Retrieves the data from local storage and commits setUser with it. If not, autologin will not be executed.
+
+    const token = localStorage.getItem("token");
+    const userId = localStorage.getItem("userId");
+    const tokenExpiration = localStorage.getItem("tokenExpiration"); // Will return a date in the future
+
+    const expiresIn = +tokenExpiration - new Date().getTime(); // difference in milisecs between the expirate date and the current date.
+
+    // Checks whether the token is expired
+
+    if (expiresIn < 0) {
+      // In this situation the token is now expired, so no autologin
+      return;
+    } else {
+      timer =
+        (() => {
+          context.dispatch("autoLogout");
+        },
+        expiresIn);
+    }
 
     if (token && userId) {
-      context.commit('setUser', {
+      context.commit("setUser", {
         token: token,
         userId: userId,
-        tokenExpiration: null,
-      })
+      });
     }
+  },
+  autoLogout(context) {
+    context.dispatch('logout');
+    context.commit('setAutoLogout');
   }
 };
